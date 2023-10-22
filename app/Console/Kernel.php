2923cddbb3f79157ca52ae98de3842f05dbf0ff9
akbarbw <akbarbint2303@gmail.com>
2023-10-22 10:48:33 +0800

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->job(new UpdateTransactionStatus)->daily();
+         $schedule->job(new UpdateTransactionStatus)->everyFiveMinutes();
 
     }
 
